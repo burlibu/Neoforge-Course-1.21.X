76592fa0ca1f0f32223f32725ac228c0631039ea
AlessandroBurlando <alessandro.burlando.studio@gmail.com>
2025-12-06 22:08:40 +0100

    Fence,wall,door,trapdoor

--- src/main/java/net/burlibu/mccourse/block/ModBlocks.java
+++ src/main/java/net/burlibu/mccourse/block/ModBlocks.java
@@ -6,9 +6,10 @@ import net.burlibu.mccourse.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.block.state.properties.WoodType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -39,7 +40,30 @@ public class ModBlocks {
             () -> new DropExperienceBlock(UniformInt.of(2,5),BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
     //
     public static final DeferredBlock<Block> MAGIC_BLOCK = registerBlock("magic_block",
-            () -> new MagicBlock(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+            () -> new MagicBlock(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().noLootTable()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_STAIRS = registerBlock("black_opal_stairs",
+            () -> new StairBlock(ModBlocks.BLACK_OPAL_BLOCK.get().defaultBlockState(),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_SLAB = registerBlock("black_opal_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_PRESSURE_PLATE = registerBlock("black_opal_pressure_plate",
+            () -> new PressurePlateBlock(BlockSetType.IRON, BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> BLACK_OPAL_BUTTON = registerBlock("black_opal_button",
+            () -> new ButtonBlock(BlockSetType.IRON, 10, BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().noCollission()));
+    public static final DeferredBlock<Block> BLACK_OPAL_FENCE = registerBlock("black_opal_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> BLACK_OPAL_FENCE_GATE = registerBlock("black_opal_fence_gate",
+            () -> new FenceGateBlock(WoodType.ACACIA, BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> BLACK_OPAL_WALL = registerBlock("black_opal_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+    public static final DeferredBlock<Block> BLACK_OPAL_DOOR = registerBlock("black_opal_door",
+            () -> new DoorBlock(BlockSetType.IRON, BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().noOcclusion()));
+    public static final DeferredBlock<Block> BLACK_OPAL_TRAPDOOR = registerBlock("black_opal_trapdoor",
+            () -> new TrapDoorBlock(BlockSetType.IRON, BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().noOcclusion()));
+    //===================================== HELPER FUNCS ================================================
     // Helper
     private static <T extends Block>DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name,block);
--- src/main/java/net/burlibu/mccourse/item/ModCreativeModeTabs.java
+++ src/main/java/net/burlibu/mccourse/item/ModCreativeModeTabs.java
@@ -27,6 +27,7 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.CHAINSAW);
                         output.accept(ModItems.TOMATO);
                         output.accept(ModItems.FROSTFIRE_ICE);
+
                     })
                     .build());
     //================================================================================================
@@ -47,6 +48,15 @@ public class ModCreativeModeTabs {
                         output.accept(ModBlocks.BLACK_OPAL_NETHER_ORE);
                         output.accept(ModBlocks.BLACK_OPAL_END_ORE);
                         output.accept(ModBlocks.MAGIC_BLOCK);
+                        output.accept(ModBlocks.BLACK_OPAL_STAIRS);
+                        output.accept(ModBlocks.BLACK_OPAL_SLAB);
+                        output.accept(ModBlocks.BLACK_OPAL_PRESSURE_PLATE);
+                        output.accept(ModBlocks.BLACK_OPAL_BUTTON);
+                        output.accept(ModBlocks.BLACK_OPAL_FENCE);
+                        output.accept(ModBlocks.BLACK_OPAL_FENCE_GATE);
+                        output.accept(ModBlocks.BLACK_OPAL_WALL);
+                        output.accept(ModBlocks.BLACK_OPAL_DOOR);
+                        output.accept(ModBlocks.BLACK_OPAL_TRAPDOOR);
                     })
                     .build());
     //======================================================================================================
