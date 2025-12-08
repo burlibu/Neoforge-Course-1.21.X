9cc9a850a2b8fd62a30a121aa57fac61106eeaba
AlessandroBurlando <alessandro.burlando.studio@gmail.com>
2025-12-08 13:51:15 +0100

    Aggiunto effetto all' armor

--- src/main/java/net/burlibu/mccourse/item/ModItems.java
+++ src/main/java/net/burlibu/mccourse/item/ModItems.java
@@ -53,7 +53,7 @@ public class ModItems {
     // ========================================================== ARMOR ========================================================================
     // =========================================================================================================================================
     public static final DeferredItem<Item> BLACK_OPAL_HELMET = ITEMS.register("black_opal_helmet",
-            () -> new ArmorItem(ModArmorMaterials.BLACK_OPAL, ArmorItem.Type.HELMET,
+            () -> new ModArmorItem(ModArmorMaterials.BLACK_OPAL, ArmorItem.Type.HELMET,
                     new Item.Properties().durability(ArmorItem.Type.HELMET.getDurability(16))));
     public static final DeferredItem<Item> BLACK_OPAL_CHESTPLATE = ITEMS.register("black_opal_chestplate",
             () -> new ArmorItem(ModArmorMaterials.BLACK_OPAL, ArmorItem.Type.CHESTPLATE,
